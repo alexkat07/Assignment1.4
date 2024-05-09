d2c62de668d70e7cf7cb12c28eb0a8f27787e489
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-09 11:44:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Assignment1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            Point point2 = new Point();
+
+            point1.x = 5.5;
+            point2.x = 5.5;
+
+            if (point1.x == point2.x) 
+            {
+                Console.WriteLine("Points are on the same axis");
+            }
+            else 
+            { 
+                Console.WriteLine("Points are not on same axis");
+            }
+        }
+        
+    }
+    struct Point
+    {
+        public double x;
+        public double y;
+
+    }
+}
